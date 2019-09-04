1900ac7035bff01067a1e7aa525161b994e17f7e
Veli Bacik <velibacik60@gmail.com>
2019-09-04 14:52:58 +0300

    intro dart complete

--- 01 - introduction/start/lib/main.dart
+++ 01 - introduction/start/lib/main.dart
@@ -5,16 +5,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String title = "Hello";
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+      home: Container(
+        child: Center(
+          child: Text(title),
         ),
       ),
     );
