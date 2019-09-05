58d126ac37707d41538a491594f9a893810247ed
Veli Bacik <velibacik60@gmail.com>
2019-09-05 11:40:05 +0300

    ppt ınstallation add 02 scaffold tutorial add

---
+++ Egzersizler/02 - scaffold/end/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Uygulamamizin  material componentleri ile kullanmak icin sarmaliyoruz.
+    return MaterialApp(
+      title: 'Material App',
+
+// Scaffold yani iskelet componeneti bizim sayfalarimizda tasarim şablonu olarak düşünebilir.
+// Bu şablon header-body-footer alanı gibi çalışmaktadır
+      home: Scaffold(
+        // appbar web dünyasındaki headerin karşılığıdır.
+        // Sizlere application sayfalarında dinamik bar tasarımlar yapmanıza olanak sağlar
+        // İçerisndeki özellikler ile istediğiniz bir çok bar tasarımını yapabilirsiniz
+        appBar: AppBar(
+          // title property'si sizden bir widget bekler yani buda buraya bir çok componenti verebilirsiniz gibi düşünebilirsiniz
+          // ex: diyelim ki centere basınca alert cıksın gibi düşünebilrisiniz
+          title: Text("Welcome StarForce"),
+          // Barımızın sol kısmında ayrılmış kısım burada genelde back button veya hamburger menu gıbı ıtemler bulunur.
+          leading: Text("Sol Bar"),
+          // Barımızın sağ kısmında ayrılmış kısım burada ise genelde action itemler bulunur dokunma anındaki eventler
+
+          actions: <Widget>[Text("Sağ Bar ")],
+        ),
+        // Sayfamızın en altındaki bar olarak footer alanı da diyebilirsiniz
+        // Burada sayfanın navigation işlemlerinin yanı sıra barınızda bir çok opsiyonu kullanabilirsiniz
+        bottomNavigationBar: BottomNavigationBar(
+          items: [BottomNavigationBarItem(icon: Icon(Icons.ac_unit))],
+        ),
+
+        // Sayfamızın ana
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
