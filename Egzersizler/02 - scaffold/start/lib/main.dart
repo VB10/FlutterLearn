07353473a094409914ecf628d329d7aeaf57458f
Veli Bacik <velibacik60@gmail.com>
2019-09-06 17:37:56 +0300

    project structure complete

--- Egzersizler/02 - scaffold/start/lib/main.dart
+++ Egzersizler/02 - scaffold/start/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return;
