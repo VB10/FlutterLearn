0c107a57d5702ec1c2e6e35fd6b1fd128131b462
Veli Bacik <velibacik60@gmail.com>
2019-09-09 17:52:19 +0300

    This repo have sample statles widgets

---
+++ Egzersizler/06 - stateless/end/lib/sample.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class SampleWidget extends StatelessWidget {
+  const SampleWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Welcome"),
+    );
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("data");
+  }
+}
+
+class ContainerCustomWidget extends StatelessWidget {
+  final Widget child;
+
+  const ContainerCustomWidget({Key key, this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: child,
+    );
+  }
+}
+
+class ContainerNullWidget extends StatelessWidget {
+  final Widget child;
+
+  const ContainerNullWidget({Key key, this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: this.child != null ? child : CircularProgressIndicator(),
+    );
+  }
+}
+
+class ContainerVisibleWidget extends StatelessWidget {
+  final Widget child;
+  final bool visible;
+
+  const ContainerVisibleWidget({Key key, this.child, @required this.visible})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Visibility(
+        visible: visible,
+        child: child,
+      ),
+    );
+  }
+}
+
+class RoundedContainer extends StatelessWidget {
+  final Widget child;
+  final int radius;
+
+  const RoundedContainer({Key key, this.child, @required this.radius})
+      : assert(radius > 0),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(radius.toDouble())),
+      child: child,
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback callback;
+  final Icon icon;
+  final String text;
+
+  const CustomButton({Key key, this.callback, this.icon, this.text})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton.icon(
+      onPressed: () {
+        // Math Progress.
+        this.callback();
+      },
+      icon: icon,
+      label: Text(text),
+    );
+  }
+}
+
