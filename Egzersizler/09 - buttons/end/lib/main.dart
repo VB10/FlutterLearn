57cce69ed8ca96ba0969c6a9cbf9e607015d4b10
Veli Bacik <velibacik60@gmail.com>
2019-09-20 10:51:34 +0300

    buttons exercise added new sample

--- Egzersizler/09 - buttons/end/lib/main.dart
+++ Egzersizler/09 - buttons/end/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: <Widget>[
+              // _samples,
               _flatButton,
               _raisedButton,
               _fabButton,
@@ -51,8 +52,42 @@ Widget get _roundedButton {
   return RaisedButton(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
     color: Colors.red,
-    
     child: Text("on press"),
     onPressed: () {},
+  );
+}
+
+Widget get _samples {
+  return Column(
+    children: <Widget>[
+      const RaisedButton(
+        onPressed: null,
+        child: Text('Disabled Button', style: TextStyle(fontSize: 20)),
+      ),
+      const SizedBox(height: 30),
+      RaisedButton(
+        onPressed: () {},
+        child: const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+      ),
+      const SizedBox(height: 30),
+      RaisedButton(
+        onPressed: () {},
+        textColor: Colors.white,
+        padding: const EdgeInsets.all(0.0),
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: <Color>[
+                Color(0xFF0D47A1),
+                Color(0xFF1976D2),
+                Color(0xFF42A5F5),
+              ],
+            ),
+          ),
+          padding: const EdgeInsets.all(10.0),
+          child: const Text('Gradient Button', style: TextStyle(fontSize: 20)),
+        ),
+      ),
+    ],
   );
 }
