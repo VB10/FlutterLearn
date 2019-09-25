ab8421f7b1744a503f38fea47021e75377c4c4b0
Veli <Bacik>
2019-09-26 02:38:06 +0300

    login view add keys and state

--- Egzersizler/login_app/end/lib/login_view.dart
+++ Egzersizler/login_app/end/lib/login_view.dart
@@ -10,6 +10,9 @@ class _LoginViewState extends State<LoginView> {
   bool isEmailFieldFocus = false;
   FocusNode _focusNode = FocusNode();
 
+  GlobalKey<FormState> key = GlobalKey(debugLabel: "keys");
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey(debugLabel: "scaffolds");
+
   @override
   void initState() {
     super.initState();
@@ -23,6 +26,7 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       body: SafeArea(
         minimum: EdgeInsets.symmetric(horizontal: 25),
         child: Column(
@@ -35,23 +39,30 @@ class _LoginViewState extends State<LoginView> {
                   color: Colors.green,
                   size: 100,
                 ))),
-            Expanded(
-                child: TextField(
-              focusNode: _focusNode,
-              onEditingComplete: () {
-                print("asd");
-              },
-              decoration: InputDecoration(
-                  labelStyle: TextStyle(
-                      color: isEmailFieldFocus ? Colors.red : Colors.grey),
-                  labelText: "Email addressasd",
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red)),
-                  suffixIcon: isEmailFieldFocus
-                      ? Icon(Icons.check_circle, color: Colors.red)
-                      : null),
-            )),
-            SizedBox(height: 30,),
+            Form(
+              key: key,
+              child: TextFormField(
+                validator: (text) {
+                  if (text.isEmpty) {
+                    return "This field is not empty";
+                  }
+                  return null;
+                },
+                focusNode: _focusNode,
+                decoration: InputDecoration(
+                    labelStyle: TextStyle(
+                        color: isEmailFieldFocus ? Colors.red : Colors.grey),
+                    labelText: "Email addressasd",
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    suffixIcon: isEmailFieldFocus
+                        ? Icon(Icons.check_circle, color: Colors.red)
+                        : null),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
             Expanded(child: TextField()),
             Expanded(
                 child: Align(
@@ -60,7 +71,19 @@ class _LoginViewState extends State<LoginView> {
             Expanded(
               child: RaisedButton(
                   padding: EdgeInsets.all(0),
-                  onPressed: () {},
+                  onPressed: () {
+                    if (key.currentState.validate()) {
+                      scaffoldKey.currentState.showSnackBar(SnackBar(
+                        content: Text("Oke"),
+                      ));
+                      return;
+                    }
+                    showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                              child: Text("false"),
+                            ));
+                  },
                   child: SizedBox.expand(
                     child: Container(
                       alignment: Alignment.center,
@@ -106,8 +129,7 @@ class _LoginViewState extends State<LoginView> {
                     TextSpan(
                         text: "Sign up", style: TextStyle(color: Colors.pink)),
                     TextSpan(
-                        text: " here ",
-                        style: TextStyle(color: Colors.black)),
+                        text: " here ", style: TextStyle(color: Colors.black)),
                   ]),
                 ))),
           ],
